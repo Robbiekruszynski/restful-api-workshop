8a42a54f75f8bb880b975de2ac18ea313a94fb16
robbie <robbiekruszynski@gmail.com>
2019-11-16 14:37:09 -0800

    success starting local at 3001

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 
 //ROUTES
+app.get("/", (req, res) => {
+  res.send("we are on our way to learning more about this!");
+});
 
 //start listening to the server
 //what you use in the arg will be where it routes to i.e. port 3000 etc...
