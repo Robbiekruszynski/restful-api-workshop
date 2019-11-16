87d9df64ff78caf2efa3ed93a847907de93f1360
robbie <robbiekruszynski@gmail.com>
2019-11-16 14:33:04 -0800

    start to set up express with route to 3001

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+//ROUTES
+
+//start listening to the server
+//what you use in the arg will be where it routes to i.e. port 3000 etc...
+app.listen(3001);
