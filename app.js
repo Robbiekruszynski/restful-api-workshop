b0b4e1afc957eef0b32acf10d8f6fdd0f138d466
robbie <robbiekruszynski@gmail.com>
2019-11-16 14:42:25 -0800

    add seperate route func

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ const express = require("express");
 const app = express();
 
 //ROUTES
+
+//get shoots back a message
+//if I wanted to do a submit (like a form I would instead of get use post)
+//there is also delete and post to add after app
 app.get("/", (req, res) => {
-  res.send("we are on our way to learning more about this!");
+  res.send("we are on our way, this is the home area!");
+});
+
+//always make sure to make the route call with a /
+app.get("/posts", (req, res) => {
+  res.send("we are on our way, this is the post area!");
 });
 
 //start listening to the server
