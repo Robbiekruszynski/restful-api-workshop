47fa7cc0e20ae18b26663180d5c4787211f293d1
robbie <robbiekruszynski@gmail.com>
2019-11-16 16:16:38 -0800

    connect mongoDB along with add env file

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require("express");
 
 const app = express();
 
-//ROUTES
+const mongoose = require("mongoose");
+
+require("dotenv/config");
+
+////////Middleware//////// = function that executes when routes are being hit
+app.use("/posts", () => {
+  console.log("hello this is middleware running");
+});
+
+////////ROUTES////////
 
 //get shoots back a message
 //if I wanted to do a submit (like a form I would instead of get use post)
@@ -15,6 +24,15 @@ app.get("/", (req, res) => {
 app.get("/posts", (req, res) => {
   res.send("we are on our way, this is the post area!");
 });
+
+//////connect to DB via hardcode
+mongoose.connect(
+  //////connect to DB via hardcode
+  process.env.DB_CONNECTION,
+  //the next line is to kill the depreactionwarning
+  { userNewUrlParser: true },
+  () => console.log("connected to DataBase")
+);
 
 //start listening to the server
 //what you use in the arg will be where it routes to i.e. port 3000 etc...
